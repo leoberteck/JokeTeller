00ba32ab6588f853ced2d333e935cd0fcdb2e476
leoberteck <stabileleonardo@hotmail.com>
2018-02-19 21:51:09 -0300

    add gitignore

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/api/EndpointsAsyncTask.java
@@ -0,0 +1,10 @@
+package com.udacity.gradle.builditbigger.api;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by leonardo on 18/02/18.
+ */
+
+public class EndpointsAsyncTask extends AsyncTask {
+}
